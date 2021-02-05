9562939e05f59dd1b4f6e6cbae06b0e2311c1f55
MohammadHH <mohammad19991998dtrb@gmail.com>
2021-02-05 23:45:25 +0200

    Add security with minimal configurations

---
+++ src/main/java/com/rrps/security/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.rrps.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/hello")
+    public String hello() {
+        return "<h1>Hello</h1>";
+    }
+}
---
+++ src/test/java/com/rrps/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrps.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
