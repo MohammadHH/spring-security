b7004335974640d8097aaa75c2ad96fb653f9811
MohammadHH <mohammad19991998dtrb@gmail.com>
2021-02-06 18:59:19 +0200

    Add authentication with jdbc, h2 data source and default DDL schema

--- src/main/java/com/rrps/security/security/SecurityConfiguration.java
+++ src/main/java/com/rrps/security/security/SecurityConfiguration.java
@@ -1,15 +1,26 @@
 package com.rrps.security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private DataSource dataSource;
+
+    @Autowired
+    public SecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
@@ -18,9 +29,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // Authentication Configuration
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().
-                withUser("user").password("12345").roles("USER").and()
-                .withUser("admin").password("12345").roles("USER", "ADMIN");
+        // make authentication based on database
+        // the datasource here is injected with spring based on classpath
+        // since h2 is present -> spring will choose it as dependency and inject it
+        // the database is built with default schema -> i.e a user and authorities table
+        auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema().withUser(
+                User.withUsername("user").password("12345").roles("USER")
+        ).withUser(
+                User.withUsername("admin").password("12345").roles("ADMIN", "USER")
+        );
     }
 
 
@@ -30,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("USER", "ADMIN")
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and().formLogin();
     }
 }
